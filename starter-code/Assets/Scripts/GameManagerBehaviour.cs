a48b6abeeebc95ef7b870d5e73f8caec94e428d3
monty1289 <64822697+monty1289@users.noreply.github.com>
2022-04-03 12:05:41 +1200

    lecture notes 3 complete

--- starter-code/Assets/Scripts/GameManagerBehaviour.cs
+++ starter-code/Assets/Scripts/GameManagerBehaviour.cs
@@ -17,10 +17,34 @@ public class GameManagerBehaviour : MonoBehaviour
         }
     
     }
+
+    public Text waveLabel;
+    public GameObject[] nextWaveLabels;
+    public bool gameOver = false;
+
+    private int wave;
+    public int Wave
+    {
+        get { return wave; }
+        set
+        {
+            wave = value;
+            if (!gameOver)
+            {
+                for (int i = 0; i < nextWaveLabels.Length; i++)
+                {
+                    nextWaveLabels[i].GetComponent<Animator>().SetTrigger("nextWave");
+                }
+            }
+            waveLabel.text = "WAVE: " + (wave + 1);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         Gold = 1000;
+        Wave = 0;
     }
 
     // Update is called once per frame
